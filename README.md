260c974a25c05d320919ba8e4449c4bb5bf4fdcc
helloshyann <helloshyann@gmail.com>
2021-08-29 21:16:33 -0700

    Blockchain Network
    Flask API and Pub/Sub

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # python-blockchain
  Python & React / Blockchain + Cryptocurreny Project
+
+**ACTIVATE VIRTUAL ENVIRONMENT**
+```
+source blockchain-env/scripts/activate
+```
+
+**INSTALL ALL PACKAGES**
+```
+pip3 install -r requirements.txt
+```
+
+**RUN THE TESTS**
+Make sure to activate the virtual environment
+```
+python3 -m pytest backend/tests
+```
+
+**RUN THE APPLICATION AND API**
+Make sure to activate the virtual environment
+```
+python -m pytest backend.app
+```
+
+**RUNNING A PEER INSTANCE**
+Activate virtual environment
+```
+export PEER=True && python -m backend.app
+```
--- backend/blockchain/block.py
+++ backend/blockchain/block.py
@@ -40,6 +40,12 @@ class Block:
     def __eq__(self, other):
         return self.__dict__ == other.__dict__
 
+    def to_json(self):
+        """
+        Serialize the block into a dictionary of it attributes
+        """
+        return self.__dict__
+
     @staticmethod
     def mine_block(last_block, data):
         """
@@ -74,6 +80,13 @@ class Block:
         #) 
 
         return Block(**GENESIS_DATA)
+
+    @staticmethod
+    def from_json(block_json):
+        """
+        Deserilize a block's json representation back into a block instance.
+        """
+        return Block(**block_json)
 
     @staticmethod
     def adjust_difficulty(last_block, new_timestamp):
