d684b5ea459a8fe1a49418c65d2d696f1fca2176
helloshyann <helloshyann@gmail.com>
2021-09-05 22:01:39 -0700

    frontend cryptocurrency

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,8 +1,9 @@
 import React, {useState, useEffect} from 'react';
+import {Link} from 'react-router-dom';
 import logo from '../assets/logo.png';
 import {API_BASE_URL} from '../config';
 
-import Blockchain from './Blockchain';
+
 
 function App() {
 
@@ -20,13 +21,15 @@ function App() {
     <div className="App">
       <img className="logo" src={logo} alt="application-logo" />
       <h3>Welcome to Pychain</h3>
+      <br/>
+      <Link to='/blockchain'>Blockchain</Link>
+      <Link to='/conduct-transaction'>Conduct a Transaction</Link>
+      <Link to='/transaction-pool'>Transaction Pool</Link>
       <br />
       <div className="WalletInfo">
         <div>Address: {address}</div>
         <div>Balance: {balance}</div>
       </div>
-      <br/>
-      <Blockchain />
     </div>
   );
 }
